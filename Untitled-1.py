209c84c18d687cf46fd17ce78a44dcbc7fe9e6dd
연태 <g9060g9060@gmail.com>
2025-03-13 18:18:26 +0900

    ✨ 영수증 보기 기능 추가
    영수증 보기 기능 추가 / 볼 상황이 없을 때 반응 추가

--- Untitled-1.py
+++ Untitled-1.py
@@ -4,14 +4,13 @@
 # 숫자로 원하는 기능을 입력할 수 있게 만들어주세요 그리고 사용자가 입력한 기능은 num 변수에 담아주세요
 # deposit_amount: 
 # withdraw_amount:
-# 영수증 기능에 사용되는 데이터 타입은 list = [["입금 , 5000, 8000"],["출금",3000,5000],["출금",2000,3000]]
+# 영수증 기능에는 사용되는 데이터타입은 list => [["입금", 5000, 8000],["출금", 3000, 5000], ["출금", 2000, 3000]]
+
+#영수증 기능 구현을 위한 빈 리스트 선언
+receipts = []
 
 #원금을 담는 변수
 balance = 3000
-
-#영수증 기능 구현을 위한 빈 리스트 선언
-receipts = [ ]
-
 
 
 while True:
@@ -26,7 +25,7 @@ while True:
             #영수증에 데이터 넣기
             receipts.append(("입금",deposit_amount, balance))
         else:
-            print("정신차리고, 제대로된 금액을 입력해라.")
+            print("정신차리고, 제대로된 숫자형태로 입금액을 작성해줘!!!")
     if num == "2":
         withdraw_amount = input("출금할 금액을 입력해주세요 : ")
         if withdraw_amount.isdigit() and int(withdraw_amount) > 0:
@@ -37,7 +36,7 @@ while True:
             #영수증에 데이터 넣기
             receipts.append(("출금",withdraw_amount, balance))
         else:
-            print("정신차리고, 제대로된 출금액을 입력해라.")
+            print("정신차리고, 제대로된 숫자형태로 입금액을 작성해줘!!!")
     if num == "3":
         #리스트에 값이 있을때와 없을때 달랐으면 합니다.
         #값이 있을때 출력 결과가 출금 : 3000원 | 잔액 : 5000원과 같이 나왔으면 합니다.
@@ -47,7 +46,7 @@ while True:
             for i in receipts:
                 print(f"{i[0]} : {i[1]}원 | 잔액 {i[2]}원")
         else:
-            print("야.... 영수증에 아무내역도 없는데 생각 좀 하고 써라.")
+            print("야.... 영수증에 아무내역도 없습니다.")
     if num == "4":
         print("서비스를 종료합니다.")
         break
